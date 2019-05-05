4023764cd2f5fdbc18816f9ebc254173f0c5bc95
zaeemasvat <z.asvat07@gmail.com>
2019-05-05 23:38:29 +0200

    moved models back into main folder

--- message_board/models.py
+++ message_board/models.py
@@ -16,36 +16,3 @@ class Message(models.Model):
     # To delete it you will have to delete all objects that reference it manually.
     # SQL equivalent: RESTRICT.
 
-
-class Club (models.Model):
-    club_id = models.IntegerField(primary_key=True)
-    club_name = models.CharField(max_length=30)
-
-
-class Portfolio (models.Model):
-    portfolio_id = models.IntegerField(primary_key=True)
-    portfolio_name = models.CharField(max_length=30)
-
-
-class StudentClubRelation(models.Model):
-    user_id = models.CharField(max_length=10)
-    club_id = models.ForeignKey(Club, on_delete=models.CASCADE)
-    portfolio_id = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
-
-
-class WitsSportExecutive(models.Model):
-    user_id = models.CharField(max_length=10)
-    portfolio_id = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
-
-
-class WitsSport(models.Model):
-    user_id = models.CharField(max_length=10)
-    portfolio_id = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
-
-
-class UserDetails(models.Model):
-    user_id = models.CharField(max_length=10)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email_address = models.CharField(max_length=30)
-    contact_number = models.IntegerField()
