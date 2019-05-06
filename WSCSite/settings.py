1cc52e133b6f8e415231d83b7a4045d7fb4e5413
Michael Nyembe <1278687@students.wits.ac.za>
2019-05-06 09:48:30 +0200

    added model test

--- WSCSite/settings.py
+++ WSCSite/settings.py
@@ -44,8 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'django.contrib.sites',
+    'django.contrib.sites',
+
     'widget_tweaks',
+
+    #my app
+    'WSCSite',
 ]
 
 SITE_ID = 1
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.db import models
+from . import models
 # Create your tests here.
+
+class TestMainModels(TestCase):
+
+    #def setUp(self):
+    #    self.club1 = Club.objects.create(
+    #        club_id = "",
+    #        club_name="Hockey"
+    #    )
+
+    #def test_club_is_created_succesfully(self):
+    #    print(self.club1.slug)
+    #    #self.assertEquals(self.club1.first(), "Hockey")
+
+    def test_user(self):
+        user = User.objects.all()
+        print(user)
