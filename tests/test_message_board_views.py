2f26bb8dd13b84c14278eeeb78128cbbda528d63
Michael Nyembe <1278687@students.wits.ac.za>
2019-05-06 08:01:26 +0200

    Created message board views tests

--- tests/test_message_board_views.py
+++ tests/test_message_board_views.py
@@ -9,6 +9,10 @@ class TestViews(TestCase):
         self.messages_url = reverse('Messages')
         self.mainmessages_url = reverse('MainMessageBoard')
         self.manageusers_url = reverse('ManageUsers')
+        self.manageclubs_url = reverse('ManageClubs')
+        self.customiseusers_url = reverse('CustomiseUsers')
+        self.sendmessage_url = reverse('SendMessage')
+        self.customiseclubs_url = reverse('CustomiseClub')
 
     #def test_AdminMessageBoard_GET(self):
     #    response = self.client.get(self.messageBoard_url)
@@ -37,3 +41,31 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         # print(response.status_code)
         self.assertTemplateUsed(response, 'message_board/Manage_Users.html')
+
+    def test_ManageClubs_GET(self):
+        response = self.client.get(self.manageclubs_url)
+        # print(response)
+        self.assertEquals(response.status_code, 200)
+        # print(response.status_code)
+        self.assertTemplateUsed(response, 'message_board/Manage_Clubs.html')
+
+    def test_CustomiseUsers_GET(self):
+        response = self.client.get(self.customiseusers_url)
+        # print(response)
+        self.assertEquals(response.status_code, 200)
+        # print(response.status_code)
+        self.assertTemplateUsed(response, 'message_board/Customise_Users.html')
+
+    def test_SendMessage_GET(self):
+        response = self.client.get(self.sendmessage_url)
+        # print(response)
+        self.assertEquals(response.status_code, 200)
+        # print(response.status_code)
+        self.assertTemplateUsed(response, 'message_board/Send_Message.html')
+
+    def test_CustomiseClubs_GET(self):
+        response = self.client.get(self.customiseclubs_url)
+        # print(response)
+        self.assertEquals(response.status_code, 200)
+        # print(response.status_code)
+        self.assertTemplateUsed(response, 'message_board/Customise_Clubs.html')
