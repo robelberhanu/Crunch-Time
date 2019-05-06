8f434bb7332314f814750f0f2f0e46492c41c97c
SimonRosen42 <simonrosen42@gmail.com>
2019-05-06 05:46:57 +0200

    Add user functionality and start of edit user functionality.

---
+++ administration/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /administration/users
+    path('users/', views.manage_users, name='manage_users'),
+    # ex: /administration/user/1391994/
+    path('users/<str:user_id>/', views.user, name='user'),
+    # ex: /administration/clubs
+    path('clubs/', views.manage_clubs, name='manage_clubs'),
+    # ex: /administration/club/Throwy_Disk/
+    path('clubs/<str:club_id>/', views.club, name='club'),
+]
