7668a1c540a06b3cc906aa8b454a6289691ab9b1
SimonRosen42 <30725995+SimonRosen42@users.noreply.github.com>
2019-05-30 01:31:35 +0200

    xD

---
+++ administration/forms.py
@@ -0,0 +1,17 @@
+# users/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# class CustomUserCreationForm(UserCreationForm):
+#
+#     class Meta(UserCreationForm):
+#         model = CustomUser
+#         fields = ('username', 'email')
+#
+# class CustomUserChangeForm(UserChangeForm):
+#
+#     class Meta:
+#         model = CustomUser
+#         fields = ('username', 'email')
+
+# UserCreationForm is in 'users\forms.py'
