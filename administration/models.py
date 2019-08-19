e43e11d36a9bd88509ca1c081a00e127863015ca
simonrosen42 <simonrosen42@gmail.com>
2019-08-19 23:13:40 +0200

    xD

--- administration/models.py
+++ administration/models.py
@@ -5,29 +5,29 @@ from django.db import models
 import users.models
 
 
-class Club(models.Model):
-    name = models.CharField(max_length=30)
-    # members = models.ManyToManyField(users.models.CustomUser, through='Membership') # Not required for now
-
-    def __str__(self):
-        return self.name
-
-
-class Position(models.Model):
-    name = models.CharField(max_length=30)
-    isExec = models.BooleanField()
-
-    def __str__(self):
-        return self.name
-
-
-class Membership(models.Model):  # Works as relational table between club and user
-    club = models.ForeignKey(Club, on_delete=models.CASCADE)
-    user = models.ForeignKey(users.models.CustomUser, on_delete=models.CASCADE)
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.club.name + "-" + self.user.username + "-" + self.position.name
-
-    class Meta:
-        unique_together = ('club', 'user',)
+# class Club(models.Model):
+#     name = models.CharField(max_length=30)
+#     # members = models.ManyToManyField(users.models.CustomUser, through='Membership') # Not required for now
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Position(models.Model):
+#     name = models.CharField(max_length=30)
+#     isExec = models.BooleanField()
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Membership(models.Model):  # Works as relational table between club and user
+#     club = models.ForeignKey(Club, on_delete=models.CASCADE)
+#     user = models.ForeignKey(users.models.CustomUser, on_delete=models.CASCADE)
+#     position = models.ForeignKey(Position, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.club.name + "-" + self.user.username + "-" + self.position.name
+#
+#     class Meta:
+#         unique_together = ('club', 'user',)
