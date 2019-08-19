d84e08279d81253dc8f4ece946ceb4464d36cb5a
simonrosen42 <simonrosen42@gmail.com>
2019-08-19 15:17:10 +0200

    1) Reverted changes to template. 2) Edited models.

--- administration/models.py
+++ administration/models.py
@@ -6,4 +6,19 @@ import users.models
 
 
 class Club(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=30)
+    members = models.ManyToManyField(users.models.CustomUser, through='Membership')
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=30)
+    isExec = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):  # Works as relational table between club and user
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    user = models.ForeignKey(users.models.CustomUser, on_delete=models.CASCADE)
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
