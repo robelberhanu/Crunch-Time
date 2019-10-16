1ec45eb1940d13bd23c69a14550a29082768dc45
zaeemasvat <z.asvat07@gmail.com>
2019-10-16 10:03:10 +0200

    updated test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,17 +3,17 @@ from users.models import CustomUser
 from main.models import Club, Portfolio, StudentClubRelation
 from users.models import CustomUser
 
-class TestModels(TestCase):
+#class TestModels(TestCase):
 
-   def test_main_Club(self):
-      clubName = Club(club_name="My Club")
-      # clubState = Club(is_deleted=)
-      self.assertEqual(str(clubName), clubName.club_name)
-      # self.assertEqual(str(clubState), clubState.is_deleted)
-
-   def test_main_Portfolio(self):
-      portName = Portfolio(portfolio_name="My Portfolio")
-      self.assertEqual(str(portName), portName.portfolio_name)
+   # def test_main_Club(self):
+   #    clubName = Club(club_name="My Club")
+   #    # clubState = Club(is_deleted=)
+   #    self.assertEqual(str(clubName), clubName.club_name)
+   #    # self.assertEqual(str(clubState), clubState.is_deleted)
+   #
+   # def test_main_Portfolio(self):
+   #    portName = Portfolio(portfolio_name="My Portfolio")
+   #    self.assertEqual(str(portName), portName.portfolio_name)
 
    # def test_main_StudentClubRelation(self):
    #    studClubRel_userid = StudentClubRelation(CustomUser.username)
